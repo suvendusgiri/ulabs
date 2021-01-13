97933818b706202e0da8ae70da04bea8a8b15c0a
Suvendu Shekhar Giri <suvendu.giri@live.com>
2021-01-13 19:49:04 +0530

    before upgrading fontawesome to v5

--- src/components/WhyChooseUs.js
+++ src/components/WhyChooseUs.js
@@ -9,81 +9,16 @@ class WhyChooseUs extends Component {
                         <div class="col-md-12">
                             <h1 class="text-center title">Why Choose Us</h1>
                             <div class="separator"></div>
-                            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            {/* <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                             <div class="row grid-space-20">
                                 <div class="col-sm-12 col-md-4 col-md-push-4">
                                     {/* <img src="assets/images/section-image-5.png" alt="" class="object-non-visible" data-animation-effect="fadeInUp" data-effect-delay="0" /> */}
                                 </div>
-                                <div class="col-sm-6 col-md-4 col-md-pull-4">
-                                    <div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="0">
-                                        <div class="icon-container default-bg">
-                                            <i class="fa fa-bold"></i>
-                                        </div>
-                                        <div class="body">
-                                            <h2>Energetic Team</h2>
-                                            <p>.</p>
-                                            {/* <a href="page-services.html" class="link"><span>Read More</span></a> */}
-                                        </div>
-                                    </div>
-                                    <div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
-                                        <div class="icon-container default-bg">
-                                            <i class="fa fa-check"></i>
-                                        </div>
-                                        <div class="body">
-                                            <h2>Collaborative</h2>
-                                            <p>Many ideas grow better when transplanted into another mind than the one where they sprang up.</p>
-                                            {/* <a href="page-services.html" class="link"><span>Read More</span></a> */}
-                                        </div>
-                                    </div>
-                                    <div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="400">
-                                        <div class="icon-container default-bg">
-                                            <i class="fa fa-bicycle"></i>
-                                        </div>
-                                        <div class="body">
-                                            <h2>Cutting-edge Technologies</h2>
-                                            <p>Inventore dolores aut laboriosam cum.</p>
-                                            {/* <a href="page-services.html" class="link"><span>Read More</span></a> */}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6 col-md-4">
-                                    <div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="0">
-                                        <div class="icon-container default-bg">
-                                            <i class="fa fa-leaf"></i>
-                                        </div>
-                                        <div class="body">
-                                            <h2>Transparency &amp; Timeliness</h2>
-                                            <p>Voluptatem ad provident non veritatis.</p>
-                                            {/* <a href="page-services.html" class="link"><span>Read More</span></a> */}
-                                        </div>
-                                    </div>
-                                    <div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
-                                        <div class="icon-container default-bg">
-                                            <i class="fa fa-laptop"></i>
-                                        </div>
-                                        <div class="body">
-                                            <h2>Friendly</h2>
-                                            <p>Iure sequi unde hic. Sapiente quaerat.</p>
-                                            {/* <a href="page-services.html" class="link"><span>Read More</span></a> */}
-                                        </div>
-                                    </div>
-                                    <div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="400">
-                                        <div class="icon-container default-bg">
-                                            <i class="fa fa-sitemap"></i>
-                                        </div>
-                                        <div class="body">
-                                            <h2>Just a call/text away</h2>
-                                            <p>Inventore dolores aut laboriosam cum.</p>
-                                            {/* <a href="page-services.html" class="link"><span>Read More</span></a> */}
-                                        </div>
-                                    </div>
-                                </div>
-
                             </div>
                             <div class="row">
                                 <div class="col-sm-4">
                                     <div class="text-center space">
-                                        <div class="box block"><i class="fa fa-leaf"></i></div>
+                                        <div class="box block"><i class="fas fa-tools"></i></div>
                                         <h2>Disruptive Innovation &amp; Cutting-edge Technologies</h2>
                                         <p class="text-muted">Highly innovative solutions require cutting edge technologies. We have Tech Experts to address any kind of technological challenegs.</p>
                                         {/* <a href="page-services.html" class="link-dark">Read More</a> */}
@@ -100,7 +35,7 @@ class WhyChooseUs extends Component {
                                 <div class="col-sm-4">
                                     <div class="text-center space">
                                         <div class="box block"><i class="fa fa-sitemap"></i></div>
-                                        <h2>Transparency, Trust &smp; Timeliness</h2>
+                                        <h2>Transparency, Trust &amp; Timeliness</h2>
                                         <p class="text-muted">These 3 Ts' are the building blocks of a positive relation. Accountability makes the bond strong further. </p>
                                         {/* <a href="page-services.html" class="link-dark">Read More</a> */}
                                     </div>
@@ -125,7 +60,7 @@ class WhyChooseUs extends Component {
                                 </div>
                                 <div class="col-sm-4">
                                     <div class="text-center space">
-                                        <div class="box block"><i class="fa fa-diamond"></i></div>
+                                        <div class="box block"><i class="fa fa-question-circle"></i></div>
                                         <h2>Just a call/text away</h2>
                                         <p class="text-muted">Inventore dolores aut laboriosam cum consequuntur delectus sequi.</p>
                                         <a href="page-services.html" class="link-dark">Read More</a>
